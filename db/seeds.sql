e3ec31eec44fc23b2cf2accf3ce991c93fce96ee
Chloe Kitts <47789998+chloeekitts@users.noreply.github.com>
2019-06-17 12:31:43 -0500

    Add files via upload

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO Burgers (id, name, devoured, date)
+VALUES
+	(1, 'Regular Hamburger', 1, '2018'),
+	(2, 'Cheese Burger', 1, '2018'),
+	(3, 'Ultra Bacon Burger', 0, '2018'),
+	(4, 'Veggie Burger', 0, '2018'),
+	(5, 'Chicken Club Burger', 0, '2018');
